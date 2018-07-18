854e00423772cb5f895c947f41cff0eea2fe74d1
Fernando Bevilacqua <dovyski@gmail.com>
2018-07-18 18:15:23 +0200

    Fix and improve RSS feed in the blog

--- blog/public/feed/index.php
+++ blog/public/feed/index.php
@@ -4,24 +4,28 @@ require_once(dirname(__FILE__) . '/../../app/App.class.php');
 
 $aApp = new App();
 $aPosts = $aApp->findPosts(true);
+$aSiteURL = 'https://blog.codebot.cc';
+$aFeedURL = $aSiteURL . '/feed/';
 
 header('Content-Type: application/rss+xml; charset=ISO-8859-1');
 
 echo '<?xml version="1.0" encoding="ISO-8859-1"?>';
-echo '<rss version="2.0">';
+echo '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">';
 echo '<channel>';
-echo '<title>My RSS feed</title>';
-echo '<link>http://www.mywebsite.com</link>';
-echo '<description>This is an example RSS feed</description>';
+echo '<atom:link href="' . $aFeedURL . '" rel="self" type="application/rss+xml" />';
+echo '<title>Codebot blog</title>';
+echo '<link>'.$aSiteURL.'</link>';
+echo '<description>Codebot is a gamedev IDE on the cloud</description>';
 echo '<language>en-us</language>';
-echo '<copyright>Copyright (C) 2009 mywebsite.com</copyright>';
+echo '<copyright>Copyright (C) '.date('Y').' blog.codebot.cc</copyright>';
 
 foreach($aPosts as $aPost) {
     echo '<item>';
         echo '<title>' . $aPost['title'] . '</title>';
-        echo '<description>' . substr($aPost['content'], 0, 400) . '</description>';
-        echo '<link></link>';
+        echo '<description><![CDATA[' . substr($aPost['content'], 0, 400) . ']]></description>';
+        echo '<link>' . $aSiteURL . '/' . basename($aPost['path']) . '</link>';
         echo '<pubDate>' . date("D, d M Y H:i:s O", strtotime($aPost['date'])) . '</pubDate>';
+        echo '<guid isPermaLink="true">' . $aSiteURL . '/' . basename($aPost['path']) . '</guid>';
     echo '</item>';
 }
 
